f896df211955c480fdbe975771686d8fc9c92bb1
hyerin <hope1053@naver.com>
2023-05-15 15:57:46 +0900

    feature: Network layer tuist 파일 추가
    feature: Network layer tuist 파일 추가

---
+++ MOITNetwork/Project.swift
@@ -0,0 +1,24 @@
+//
+//  Project.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 최혜린 on 2023/05/15.
+//
+
+import ProjectDescription
+import ProjectDescriptionHelpers
+import UtilityPlugin
+
+let project = Project.invertedDualTargetProject(
+	name: "MOITNetwork",
+	platform: .iOS,
+	iOSTargetVersion: "16.0.0",
+	interfaceDependencies: [
+		.ThirdParty.RxSwift,
+		.ThirdParty.RxCocoa,
+	],
+	implementDependencies: [
+		.ThirdParty.RxSwift,
+		.ThirdParty.RxCocoa,
+	]
+)
