790705bbc0266b465c3d05232b765c5702131ef3
SongSeoYoung <42825223+SongSeoYoung@users.noreply.github.com>
2023-06-02 09:42:57 +0900

    fix: scenedelegate사용안하도록 수정 (#11)

--- App/Project.swift
+++ App/Project.swift
@@ -30,14 +30,6 @@ let project = Project(
                         ],
                         "UIApplicationSceneManifest": [
                             "UIApplicationSupportsMultipleScenes": false,
-                            "UISceneConfigurations": [
-                                "UIWindowSceneSessionRoleApplication": [
-                                    [
-                                        "UISceneConfigurationName": "Default Configuration",
-                                        "UISceneDelegateClassName": "$(PRODUCT_MODULE_NAME).SceneDelegate"
-                                    ],
-                                ]
-                            ]
                         ],
                         "UIBackgroundModes": [
                             "fetch",
