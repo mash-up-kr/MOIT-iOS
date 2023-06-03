523e64267b9e96f78663d70e60a2374369226a65
SongSeoYoung <42825223+SongSeoYoung@users.noreply.github.com>
2023-06-03 17:31:33 +0900

    impl: 데모앱 개선 (#20)

---
+++ DesignSystem/DemoApp/Sources/DesignSystemViewController.swift
@@ -0,0 +1,68 @@
+//
+//  DesignSystemViewController.swift
+//
+//  MOIT
+//
+//  Created by kimchansoo on .
+//
+
+import UIKit
+
+fileprivate enum DesignSystemType: String,
+                                   CaseIterable {
+    case navigation
+    case list
+    case control_tab
+    case input
+    case button
+    case card
+    case modal
+}
+
+final class DesignSystemViewController: UITableViewController {
+    
+    private var designSystems: [DesignSystemType] = DesignSystemType.allCases
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationBar.largeContentTitle = "MOIT design System"
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        designSystems.count
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = self.designSystems[indexPath.item].rawValue
+        return cell
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        heightForRowAt indexPath: IndexPath
+    ) -> CGFloat {
+        return 50
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        didSelectRowAt indexPath: IndexPath
+    ) {
+        let designSystem = self.designSystems[indexPath.item]
+        switch designSystem {
+            // TODO: 여기에서 각자 화면 만드시오 !
+        default: return
+        }
+    }
+}
+
