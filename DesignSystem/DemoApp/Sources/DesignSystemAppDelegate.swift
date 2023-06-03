42571c6a5d5e35a13066f5022d66de77d0b4cc0b
SongSeoYoung <seoyoung_00@hufs.ac.kr>
2023-06-03 17:29:42 +0900

    impl: 데모앱 개선

--- DesignSystem/DemoApp/Sources/DesignSystemAppDelegate.swift
+++ DesignSystem/DemoApp/Sources/DesignSystemAppDelegate.swift
@@ -13,7 +13,8 @@ final class DesignSystemAppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = DesignSystemViewController()
+        
+        window.rootViewController = UINavigationController(rootViewController: DesignSystemViewController())
         window.makeKeyAndVisible()
         self.window = window
         return true
