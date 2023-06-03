edbfe37f413862777027c9229bec9cd9a2e1ff5c
chansooo <kcs05008@gmail.com>
2023-06-03 17:01:02 +0900

    feat: DesignSystem 구현

---
+++ DesignSystem/DemoApp/Sources/DesignSystemAppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  MainAppDelegate.swift
+//
+//  Main
+//
+//  Created by kimchansoo on .
+//
+
+import UIKit
+
+@main
+final class DesignSystemAppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = MainUserInterfaceViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
+    }
+}
+
