92ce6c0b9454f37e988f23b26c285e283aca8df8
chansooooo <chansoo@vaultmicro.com>
2023-06-05 15:46:05 +0900

    add: `MOITTabPager` 생성

---
+++ DesignSystem/Sources/TapPager/MOITTabPager.swift
@@ -0,0 +1,26 @@
+//
+//  MOITTabPager.swift
+//  DesignSystem
+//
+//  Created by 김찬수 on 2023/06/05.
+//  Copyright © 2023 chansoo.MOIT. All rights reserved.
+//
+
+import UIKit
+
+import ResourceKit
+
+import RxSwift
+import RxGesture
+
+final class MOITTabPager: UIViewController {
+    // MARK: - UI
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializers
+    
+    // MARK: - Lifecycle
+    
+    // MARK: - Functions
+}
