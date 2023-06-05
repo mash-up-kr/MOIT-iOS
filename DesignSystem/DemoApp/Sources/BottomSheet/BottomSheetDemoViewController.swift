cafc4c7485bc9656dc40347a43f47dc958e20a38
SongSeoYoung <seoyoung_00@hufs.ac.kr>
2023-06-06 01:36:34 +0900

    feat: bottom sheet

--- DesignSystem/DemoApp/Sources/BottomSheet/BottomSheetDemoViewController.swift
+++ DesignSystem/DemoApp/Sources/BottomSheet/BottomSheetDemoViewController.swift
@@ -13,15 +13,52 @@ import DesignSystem
 
 import FlexLayout
 import PinLayout
+import RxSwift
 
 final class BottomSheetDemoViewController: UIViewController {
 
     private let flexRootView = UIView()
+    private let button = UIButton()
+    private let debugLabel = UILabel()
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .blue
         self.configureLayouts()
+        self.button.setTitle("궁금하면 클릭해보시오@!", for: .normal)
+        self.button.addTarget(self, action: #selector(self.didTapButton(_:)), for: .touchUpInside)
+    }
+    
+    @objc
+    private func didTapButton(_ sender: UIButton) {
+        let view = MOITShareView(invitationCode: "MZ9750")
+        let vc = BottomSheetViewController(contentView: view)
         
+        vc.rx.didTapDimmedView
+            .bind(onNext: { [weak self] in
+                self?.dismiss(animated: true)
+            })
+            .disposed(by: self.disposeBag)
+        
+        view.rx.didTapLinkCopyButton
+            .bind(onNext: { [weak self] in
+                self?.debugLabel.text = "링크 복사하기 클릭됨 ~"
+                self?.debugLabel.flex.markDirty()
+                self?.view.setNeedsLayout()
+            })
+            .disposed(by: self.disposeBag)
+        
+        view.rx.didTapKakaoTalkShareButton
+            .bind(onNext: { [weak self] in
+                self?.debugLabel.text = "카카오톡으로 공유하기 클릭됨 ~"
+                self?.debugLabel.flex.markDirty()
+                self?.view.setNeedsLayout()
+            })
+            .disposed(by: self.disposeBag)
+        
+        self.present(vc, animated: true)
     }
     
     override func viewDidLayoutSubviews() {
@@ -32,10 +69,19 @@ final class BottomSheetDemoViewController: UIViewController {
     
     private func configureLayouts() {
         self.view.addSubview(self.flexRootView)
-        
+        self.debugLabel.textColor = .white
         self.flexRootView.flex
             .define { flex in
-                flex.addItem(MOITShareView(invitationCode: "MZ9750"))
+                
+                flex.addItem(self.debugLabel)
+                    .alignSelf(.center)
+                    .height(20)
+                
+                flex.addItem(MOITShareView(invitationCode: "저ㄴㅈr"))
+                    .marginTop(10)
+                
+                flex.addItem(self.button)
+                    .marginTop(40)
             }
     }
 }
--- DesignSystem/Sources/BottomSheet/BottomSheetViewController.swift
+++ DesignSystem/Sources/BottomSheet/BottomSheetViewController.swift
@@ -13,22 +13,78 @@ import ResourceKit
 
 import FlexLayout
 import PinLayout
+import RxSwift
+import RxCocoa
+import RxGesture
 
 public final class BottomSheetViewController: UIViewController {
     
     private let flexRootView = UIView()
     private let contentRootView = UIView()
-    private let dimmedView = UIView()
+    private let contentView: UIView
+    fileprivate let dimmedView = UIView()
     
     public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         self.view.addSubview(self.flexRootView)
         self.flexRootView.pin.all()
         self.flexRootView.flex.layout()
+        self.contentView.pin.top(20)
+            .left()
+            .right()
+            .bottom()
+    }
+    
+    public init(contentView: UIView) {
+        self.contentView = contentView
+        super.init(nibName: nil, bundle: nil)
+        self.modalPresentationStyle = .overFullScreen
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureLayouts()
+        self.contentRootView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        self.contentRootView.layer.cornerRadius = 40
+        self.contentRootView.clipsToBounds = true
+        self.contentRootView.backgroundColor = .white
+    }
+    
+    private func configureLayouts() {
+        self.view.addSubview(self.flexRootView)
+        self.dimmedView.backgroundColor = UIColor(red: 24/255, green: 24/255, blue: 24/255, alpha: 0.5)
+        
+        self.flexRootView.backgroundColor = .clear
+        self.flexRootView.flex
+            .define { flex in
+                flex.addItem(self.dimmedView)
+                    .define { flex in
+                        flex.addItem()
+                            .grow(1)
+                        
+                        flex.addItem(self.contentRootView)
+                            .define { flex in
+                                flex.addItem(self.contentView)
+                                    .position(.absolute)
+                            }
+                            .grow(1)
+                            .marginBottom(0)
+                    }
+                    .grow(1)
+            }
+    }
+}
+
+extension Reactive where Base: BottomSheetViewController {
+    public var didTapDimmedView: Observable<Void> {
+        self.base.dimmedView.rx.tapGesture()
+            .when(.recognized)
+            .throttle(.milliseconds(400), latest: false, scheduler: MainScheduler.instance)
+            .map { _ in return }
     }
 }
 
@@ -41,14 +97,14 @@ public final class MOITShareView: UIView {
     private let invitationCodeBackgroundView = UIView()
     private let invitationCodeTitleLabel = UILabel()
     private let invitationCodeLabel = UILabel()
-    private let kakaoTalkShareButton = MOITButton(
+    fileprivate let kakaoTalkShareButton = MOITButton(
         type: .large,
         image: ResourceKitAsset.Icon.kakaotalk.image,
         title: "카카오톡으로 공유하기",
         titleColor: .black,
         backgroundColor: ResourceKitAsset.Color.kakao.color
     )
-    private let linkCopyButton = MOITButton(
+    fileprivate let linkCopyButton = MOITButton(
         type: .large,
         image: ResourceKitAsset.Icon.link.image,
         title: "링크 복사하기",
@@ -144,5 +200,14 @@ public final class MOITShareView: UIView {
                     }
                     
             }
+    }
+}
+
+extension Reactive where Base: MOITShareView {
+    public var didTapKakaoTalkShareButton: Observable<Void> {
+        self.base.kakaoTalkShareButton.rx.tap
+    }
+    public var didTapLinkCopyButton: Observable<Void> {
+        self.base.linkCopyButton.rx.tap
     }
 }
