81531e903cd8c71959935643b63a33ac5bf848c6
hyerin <hope1053@naver.com>
2023-06-05 16:06:37 +0900

    feat: MOITTextFieldDemoViewController 구현

---
+++ DesignSystem/DemoApp/Sources/MOITTextFieldDemoViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MOITTextFieldDemoViewController.swift
+//  DesignSystemDemoApp
+//
+//  Created by 최혜린 on 2023/06/05.
+//  Copyright © 2023 chansoo.MOIT. All rights reserved.
+//
+
+import UIKit
+
+import DesignSystem
+
+import RxCocoa
+import RxSwift
+
+final class MOITTextFieldDemoViewController: UIViewController {
+
+	private let flexRootView = UIView()
+	private let emptyTextField = MOITTextField(
+		title: "emptyTextField",
+		placeHolder: "비어있는 텍스트 필드 테스트입니다"
+	)
+	private let label = UILabel()
+
+	private let disposeBag = DisposeBag()
+
+// MARK: - override
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		configureLayout()
+		bind()
+	}
+
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+
+		flexRootView.pin.all(view.pin.safeArea)
+		flexRootView.flex.layout(mode: .adjustHeight)
+	}
+
+// MARK: - private func
+	private func configureLayout() {
+		view.backgroundColor = .white
+		view.addSubview(flexRootView)
+		flexRootView.flex
+			.margin(20)
+			.direction(.column)
+			.define { flex in
+				flex.addItem(emptyTextField)
+				flex.addItem(label).height(20).marginTop(30)
+			}
+	}
+
+	private func bind() {
+		emptyTextField.textFieldInput
+			.bind(to: label.rx.text)
+			.disposed(by: disposeBag)
+	}
+}
