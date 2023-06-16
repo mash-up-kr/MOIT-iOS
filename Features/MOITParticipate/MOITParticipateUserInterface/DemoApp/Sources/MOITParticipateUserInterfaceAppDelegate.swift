f4dad0c63c8311e1e8f343f646aa92c03641f1da
hyerin <hope1053@naver.com>
2023-06-16 15:43:05 +0900

    feat: Demoapp 수정

--- Features/MOITParticipate/MOITParticipateUserInterface/DemoApp/Sources/MOITParticipateUserInterfaceAppDelegate.swift
+++ Features/MOITParticipate/MOITParticipateUserInterface/DemoApp/Sources/MOITParticipateUserInterfaceAppDelegate.swift
@@ -13,7 +13,7 @@ final class MOITParticipateAppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = MOITParticipateUserInterfaceViewController()
+		window.rootViewController = UINavigationController(rootViewController: MOITParticipateUserInterfaceViewController())
         window.makeKeyAndVisible()
         self.window = window
         return true
--- Features/MOITParticipate/MOITParticipateUserInterface/DemoApp/Sources/MOITParticipateUserInterfaceViewController.swift
+++ Features/MOITParticipate/MOITParticipateUserInterface/DemoApp/Sources/MOITParticipateUserInterfaceViewController.swift
@@ -10,19 +10,57 @@ import UIKit
 
 import MOITParticipateUserInterfaceImpl
 
-final class MOITParticipateUserInterfaceViewController: UIViewController {
+fileprivate enum MOITParticipateType: String,
+								   CaseIterable {
+	case inputCode
+	case particiaptionSuccess
+}
+
+final class MOITParticipateUserInterfaceViewController: UITableViewController {
+	
+	private var moitParticipateTypes: [MOITParticipateType] = MOITParticipateType.allCases
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemPink
-    }
-	
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
+        self.view.backgroundColor = .white
 		
-		let viewController = InputParticipateCodeViewController()
-		viewController.modalPresentationStyle = .fullScreen
-		self.present(viewController, animated: true)
+		self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+	override func tableView(
+		_ tableView: UITableView,
+		numberOfRowsInSection section: Int
+	) -> Int {
+		moitParticipateTypes.count
+	}
+	
+	override func tableView(
+		_ tableView: UITableView,
+		cellForRowAt indexPath: IndexPath
+	) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		cell.textLabel?.text = self.moitParticipateTypes[indexPath.item].rawValue
+		return cell
+	}
+	
+	override func tableView(
+		_ tableView: UITableView,
+		heightForRowAt indexPath: IndexPath
+	) -> CGFloat {
+		return 50
+	}
+	
+	override func tableView(
+		_ tableView: UITableView,
+		didSelectRowAt indexPath: IndexPath
+	) {
+		let moitParticipateType = self.moitParticipateTypes[indexPath.item]
+		switch moitParticipateType {
+		case .inputCode:
+			self.navigationController?.pushViewController(InputParticipateCodeViewController(), animated: true)
+		case .particiaptionSuccess:
+			break
+		}
 	}
 }
 
