59e86debabe413b9a2aadd8480b9dd7505f2894f
chansooo <kcs05008@gmail.com>
2023-06-19 11:16:05 +0900

    feat: ViewControllable extension 추가

---
+++ Core/Utils/RIBs/ViewControllable+.swift
@@ -0,0 +1,62 @@
+//
+//  ViewControllable+.swift
+//  CSLogger
+//
+//  Created by 김찬수 on 2023/06/16.
+//
+
+import UIKit
+
+import RIBs
+
+public extension ViewControllable {
+    
+    func present(_ viewControllable: ViewControllable, animated: Bool, completion: (() -> Void)?) {
+        self.uiviewController.present(viewControllable.uiviewController, animated: animated, completion: completion)
+    }
+    
+    func dismiss(completion: (() -> Void)?) {
+        self.uiviewController.dismiss(animated: true, completion: completion)
+    }
+    
+    func pushViewController(_ viewControllable: ViewControllable, animated: Bool) {
+        if let nav = self.uiviewController as? UINavigationController {
+            nav.pushViewController(viewControllable.uiviewController, animated: animated)
+        } else {
+            self.uiviewController.navigationController?.pushViewController(viewControllable.uiviewController, animated: animated)
+        }
+    }
+    
+    func popViewController(animated: Bool) {
+        if let nav = self.uiviewController as? UINavigationController {
+            nav.popViewController(animated: animated)
+        } else {
+            self.uiviewController.navigationController?.popViewController(animated: animated)
+        }
+    }
+    
+    func popToRoot(animated: Bool) {
+        if let nav = self.uiviewController as? UINavigationController {
+            nav.popToRootViewController(animated: animated)
+        } else {
+            self.uiviewController.navigationController?.popToRootViewController(animated: animated)
+        }
+    }
+    
+    func setViewControllers(_ viewControllerables: [ViewControllable]) {
+        if let nav = self.uiviewController as? UINavigationController {
+            nav.setViewControllers(viewControllerables.map(\.uiviewController), animated: true)
+        } else {
+            self.uiviewController.navigationController?.setViewControllers(viewControllerables.map(\.uiviewController), animated: true)
+        }
+    }
+    
+    var topViewControllable: ViewControllable {
+        var top: ViewControllable = self
+        
+        while let presented = top.uiviewController.presentedViewController as? ViewControllable {
+            top = presented
+        }
+        return top
+    }
+}
