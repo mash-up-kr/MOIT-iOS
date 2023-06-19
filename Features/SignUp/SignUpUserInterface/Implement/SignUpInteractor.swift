faf06de08a8464ae5e2b0f776cb2d87c902670c4
chansooo <kcs05008@gmail.com>
2023-06-19 15:36:15 +0900

    feat: SignUp 비즈니스 로직 수정

--- Features/SignUp/SignUpUserInterface/Implement/SignUpInteractor.swift
+++ Features/SignUp/SignUpUserInterface/Implement/SignUpInteractor.swift
@@ -7,17 +7,24 @@
 //
 
 import SignUpUserInterface
+import SignUpData
+import SignUpDomain
 
 import RIBs
 import RxSwift
+import RxCocoa
 
 protocol SignUpRouting: ViewableRouting {
-    // TODO: Declare methods the interactor can invoke to manage sub-tree via the router.
+    
+    func attachMOITList()
+    func attachProfileSelect()
 }
 
 protocol SignUpPresentable: Presentable {
+    
     var listener: SignUpPresentableListener? { get set }
-    // TODO: Declare methods the interactor can invoke the presenter to present data.
+    
+    func updateProfileIndex(index: Int)
 }
 
 protocol SignUpInteractorDependency {
@@ -27,28 +34,87 @@ protocol SignUpInteractorDependency {
 
 final class SignUpInteractor: PresentableInteractor<SignUpPresentable>, SignUpInteractable {
     
+    // MARK: - Properties
     weak var router: SignUpRouting?
     weak var listener: SignUpListener?
-
-    // TODO: Add additional dependencies to constructor. Do not perform any logic
-    // in constructor.
-    override init(presenter: SignUpPresentable) {
+    
+    private let dependency: SignUpInteractorDependency
+    
+    private let nickName = PublishRelay<String>()
+    private let inviteCode = PublishRelay<String>()
+    private let nextButtonTapped = PublishRelay<Void>()
+    
+    // MARK: - Initializers
+    public init(
+        presenter: SignUpPresentable,
+        dependency: SignUpInteractorDependency
+    ) {
+        self.dependency = dependency
         super.init(presenter: presenter)
         presenter.listener = self
     }
-
+    
+    // MARK: - Lifecycle
     override func didBecomeActive() {
         super.didBecomeActive()
-        // TODO: Implement business logic here.
+        
+        bind()
+        presenter.updateProfileIndex(index: dependency.fetchRandomNumberUseCase.execute(with: 0 ..< 8))
     }
-
+    
     override func willResignActive() {
         super.willResignActive()
         // TODO: Pause any business logic.
     }
     
+    // MARK: - Functions
+    private func bind() {
+        // nextButtonTapped가 발동 시 nickname과 inviteCode 스트림을 합쳐서 postJoinInfoUseCase에 전달
+        nextButtonTapped
+            .withLatestFrom(Observable.combineLatest(nickName, inviteCode))
+            .flatMap { [weak self] nickName, inviteCode -> Single<Int> in
+                guard let self = self else { return }
+                // id 반환
+                print("nickname: \(nickName)")
+                print("inviteCode: \(inviteCode)")
+                // TODO: - 받은 내 ID 저장
+                return self.dependency.postJoinInfoUseCase.execute(name: nickName, inviteCode: inviteCode)
+            }
+            .subscribe(onNext: {})
+//            .subscribe(onNext: { [weak self] nickName, inviteCode in
+//                guard let self = self else { return }
+//                // id 반환
+//                print("nickname: \(nickName)")
+//                print("inviteCode: \(inviteCode)")
+//                // TODO: - 받은 내 ID 저장
+//                self.dependency.postJoinInfoUseCase.execute(name: nickName, inviteCode: inviteCode)
+//            })
+            .disposeOnDeactivate(interactor: self)
+            
+    }
+}
+
+// MARK: - SignUpPresentableListener
+extension SignUpInteractor: SignUpPresentableListener {
+    
     func didSwipeBack() {
         
     }
-
+    
+    func didTapNextButton() {
+        nextButtonTapped.accept(())
+    }
+    
+    func didTapProfileView() {
+        router?.attachProfileSelect()
+    }
+    
+    func didTypeName(name: String) {
+        self.nickName.accept(name)
+    }
+    
+    func didTypeInviteCode(inviteCode: String) {
+        self.inviteCode.accept(inviteCode)
+    }
 }
+
