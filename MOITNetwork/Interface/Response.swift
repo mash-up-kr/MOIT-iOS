830a304d8a082a39f033a1172e8010fdb675ed61
hyerin <hope1053@naver.com>
2023-06-20 11:04:29 +0900

    feat: Response 모델 선언

---
+++ MOITNetwork/Interface/Response.swift
@@ -0,0 +1,20 @@
+//
+//  Response.swift
+//  MOITNetwork
+//
+//  Created by 최혜린 on 2023/06/19.
+//  Copyright © 2023 chansoo.MOIT. All rights reserved.
+//
+
+import Foundation
+
+public struct MOITErrorResponse: Decodable {
+	let code: String
+	let message: String
+}
+
+public struct MOITResponse<R>: Decodable where R: Decodable {
+	public let success: Bool
+	public let data: R
+	public let error: MOITErrorResponse
+}
