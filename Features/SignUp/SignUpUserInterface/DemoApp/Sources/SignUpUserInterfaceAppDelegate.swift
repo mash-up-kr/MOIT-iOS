7c54b51776c4f98bed7dabafcf45d4aee8828f80
chansooooo <chansoo@vaultmicro.com>
2023-06-21 11:06:21 +0900

    fix: interactor.activate 추가

--- Features/SignUp/SignUpUserInterface/DemoApp/Sources/SignUpUserInterfaceAppDelegate.swift
+++ Features/SignUp/SignUpUserInterface/DemoApp/Sources/SignUpUserInterfaceAppDelegate.swift
@@ -14,16 +14,19 @@ import SignUpUserInterface
 import RIBs
 
 @main
-final class SignUpAppDelegate: UIResponder, UIApplicationDelegate, SignUpListener {
+final class SignUpAppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+    var router: ViewableRouting?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
         
         let signUpBuilder = SignUpBuilder(dependency: MOCKSignUpComponent())
-        let router = signUpBuilder.build(withListener: MOCKMOITSignUpListener())
+        self.router = signUpBuilder.build(withListener: MOCKMOITSignUpListener())
 //        let launchRouter = LaunchRouter(interactor: router, viewController: router.viewControllable)
-        router.load()
-        window.rootViewController = router.viewControllable.uiviewController
+        router?.load()
+        router?.interactable.activate() // TODO: - 이 녀석의 정체는?!
+        window.rootViewController = router?.viewControllable.uiviewController
 
         window.makeKeyAndVisible()
         self.window = window
