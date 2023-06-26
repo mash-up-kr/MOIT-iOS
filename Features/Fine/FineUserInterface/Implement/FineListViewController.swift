9c4d9286d0cef3d2fc31a3d2b9d7561fa11c919d
hyerin <hope1053@naver.com>
2023-06-26 10:54:53 +0900

    design: FineListViewController 상단 디자인 구현

--- Features/Fine/FineUserInterface/Implement/FineListViewController.swift
+++ Features/Fine/FineUserInterface/Implement/FineListViewController.swift
@@ -6,13 +6,113 @@
 //  Copyright © 2023 chansoo.MOIT. All rights reserved.
 //
 
+import UIKit
+
+import ResourceKit
+import DesignSystem
+
 import RIBs
 import RxSwift
-import UIKit
+import FlexLayout
+import PinLayout
 
 protocol FineListPresentableListener: AnyObject { }
 
 final class FineListViewController: UIViewController, FineListPresentable, FineListViewControllable {
 
+// MARK: - UI
+	
+	private let flexRootContainer = UIView()
+	
+	private let fineTitleLabel: UILabel = {
+		let label = UILabel()
+		label.text = StringResource.title.value
+		label.font = ResourceKitFontFamily.h5
+		label.textColor = ResourceKitAsset.Color.gray700.color
+		return label
+	}()
+	
+	private let fineAmountLabel: UILabel = {
+		let label = UILabel()
+		label.font = ResourceKitFontFamily.h2
+		label.textColor = ResourceKitAsset.Color.gray900.color
+		return label
+	}()
+	
+	private let fineUnitLabel: UILabel = {
+		let label = UILabel()
+		label.text = StringResource.unit.value
+		label.font = ResourceKitFontFamily.h3
+		label.textColor = ResourceKitAsset.Color.gray900.color
+		return label
+	}()
+	
+	private let segmentPager = MOITSegmentPager(
+		pages: [StringResource.defaulter.value, StringResource.paymentList.value]
+	)
+	
+	
+// MARK: - property
+	
     weak var listener: FineListPresentableListener?
+	
+// MARK: - override
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		configureView()
+		configureLayout()
+	}
+	
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		
+		flexRootContainer.pin.all(view.pin.safeArea)
+		flexRootContainer.flex.layout()
+	}
+	
+// MARK: - private
+	
+	private func configureView() {
+		view.backgroundColor = .white
+		view.addSubview(flexRootContainer)
+	}
+	
+	private func configureLayout() {
+		flexRootContainer.flex.define { flex in
+			
+			flex.addItem().marginTop(20).define { flex in
+				flex.addItem(fineTitleLabel)
+				flex.addItem().direction(.row).define { flex in
+					flex.addItem(fineAmountLabel)
+					flex.addItem(fineUnitLabel)
+				}
+				
+				flex.addItem(segmentPager).marginTop(20)
+			}
+		}
+	}
+}
+
+extension FineListViewController {
+	enum StringResource {
+		case title
+		case unit
+		case defaulter
+		case paymentList
+		
+		var value: String {
+			switch self {
+			case .title:
+				return "오늘까지 모인 벌금"
+			case .unit:
+				return "원"
+			case .defaulter:
+				return "벌금미납자"
+			case .paymentList:
+				return "납부 내역"
+			}
+		}
+	}
 }
