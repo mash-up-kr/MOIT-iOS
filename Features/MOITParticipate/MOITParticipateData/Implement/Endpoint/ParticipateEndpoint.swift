829b85b3b93946fa6bb08a832353de287910ccd6
hyerin <hope1053@naver.com>
2023-07-03 17:35:24 +0900

    feat: MOITParticipate Data 레이어 작업

---
+++ Features/MOITParticipate/MOITParticipateData/Implement/Endpoint/ParticipateEndpoint.swift
@@ -0,0 +1,24 @@
+//
+//  ParticipateEndpoint.swift
+//  MOITParticipateDataImpl
+//
+//  Created by 최혜린 on 2023/07/03.
+//  Copyright © 2023 chansoo.MOIT. All rights reserved.
+//
+
+import Foundation
+
+import MOITNetwork
+import MOITParticipateData
+
+struct ParticipateEndpoint {
+	
+	func postParticipateCode(with request: MOITParticipateRequest) -> Endpoint<MOITParticipateDTO> {
+		Endpoint(
+			path: "moit/join",
+			method: .post,
+			headers: [:],
+			parameters: .body(request)
+		)
+	}
+}
---
+++ Features/MOITParticipate/MOITParticipateData/Interface/Response/MOITParticipateDTO.swift
@@ -0,0 +1,13 @@
+//
+//  MOITParticipateDTO.swift
+//  MOITParticipateDataImpl
+//
+//  Created by 최혜린 on 2023/07/03.
+//  Copyright © 2023 chansoo.MOIT. All rights reserved.
+//
+
+import Foundation
+
+public struct MOITParticipateDTO: Decodable {
+	public let moitId: Int
+}
