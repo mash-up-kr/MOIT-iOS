a4c0a05f9a6a2b37ab3ca08285edb9c1b5c703ca
chansooo <kcs05008@gmail.com>
2023-07-15 15:37:17 +0900

    feat: MOITListDemoApp DemoApp 연결

--- Features/MOITList/MOITListUserInterface/DemoApp/Sources/MOITListUserInterfaceAppDelegate.swift
+++ Features/MOITList/MOITListUserInterface/DemoApp/Sources/MOITListUserInterfaceAppDelegate.swift
@@ -8,15 +8,38 @@
 
 import UIKit
 
+import MOITListUserInterface
+import MOITListUserInterfaceImpl
+
+import RIBs
+
 @main
 final class MOITListAppDelegate: UIResponder, UIApplicationDelegate {
+    
     var window: UIWindow?
+    private var router: ViewableRouting?
+    let dependency = MockMOITListDependency()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
-        window.rootViewController = MOITListUserInterfaceViewController()
+        
+        self.router = MOITListBuilder(dependency: dependency)
+            .build(withListener: MOCKMOITListListener())
+        self.router?.load()
+        self.router?.interactable.activate()
+        window.rootViewController = self.router?.viewControllable.uiviewController
         window.makeKeyAndVisible()
         self.window = window
         return true
     }
+    
 }
 
+extension MOITListAppDelegate {
+
+    final class MockMOITListDependency:  MOITListDependency {
+        
+    }
+    private final class MOCKMOITListListener: MOITListListener {
+    }
+}
