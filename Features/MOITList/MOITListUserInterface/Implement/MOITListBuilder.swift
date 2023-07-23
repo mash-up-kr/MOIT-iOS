c3e9911141bf8dfab739497be52b511dfd8d72fe
chansooo <kcs05008@gmail.com>
2023-07-23 19:08:34 +0900

    feat: 필요한 usecase 추가

--- Features/MOITList/MOITListUserInterface/Implement/MOITListBuilder.swift
+++ Features/MOITList/MOITListUserInterface/Implement/MOITListBuilder.swift
@@ -7,26 +7,35 @@
 //
 
 import MOITListUserInterface
+import MOITListDomain
 
 import RIBs
 
-final class MOITListComponent: Component<MOITListDependency> {
-
-    // TODO: Declare 'fileprivate' dependencies that are only used by this RIB.
+final class MOITListComponent: Component<MOITListDependency>,
+                               MOITListInteractorDependency
+{
+    var fetchLeftTimeUseCase: FetchLeftTimeUseCase { dependency.fetchLeftTimeUseCase }
+    
+    var fetchPaneltyToBePaiedUSeCase: FetchPenaltyToBePaidUseCase { dependency.fetchPaneltyToBePaiedUSeCase }
+    
+    var fetchMOITListsUseCase: FetchMoitListUseCase { dependency.fetchMOITListsUseCase }
 }
 
 // MARK: - Builder
 
 public final class MOITListBuilder: Builder<MOITListDependency>, MOITListBuildable {
-
+    
     public override init(dependency: MOITListDependency) {
         super.init(dependency: dependency)
     }
-
+    
     public func build(withListener listener: MOITListListener) -> ViewableRouting {
         let component = MOITListComponent(dependency: dependency)
         let viewController = MOITListViewController()
-        let interactor = MOITListInteractor(presenter: viewController)
+        let interactor = MOITListInteractor(
+            presenter: viewController,
+            dependency: component
+        )
         interactor.listener = listener
         return MOITListRouter(interactor: interactor, viewController: viewController)
     }
