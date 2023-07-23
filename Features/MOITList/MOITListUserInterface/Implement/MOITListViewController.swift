f302c4f7989773114e807661db2507d379d76120
chansooo <kcs05008@gmail.com>
2023-07-23 19:09:04 +0900

    feat: MOITListViewController에서 보여주는 부분 구현

--- Features/MOITList/MOITListUserInterface/Implement/MOITListViewController.swift
+++ Features/MOITList/MOITListUserInterface/Implement/MOITListViewController.swift
@@ -6,25 +6,68 @@
 //  Copyright © 2023 chansoo.MOIT. All rights reserved.
 //
 
+import DesignSystem
 import MOITListUserInterface
+import MOITListDomain
 import Utils
 
+import ResourceKit
 import RIBs
 import RxSwift
 import UIKit
+import FlexLayout
+import PinLayout
 
 protocol MOITListPresentableListener: AnyObject {
-    // TODO: Declare properties and methods that the view controller can invoke to perform
-    // business logic, such as signIn(). This protocol is implemented by the corresponding
-    // interactor class.
+    
+    func didTapMOIT(index: Int) -> Void // MOIT 하나 탭 시 불리는 함수
 }
 
 final class MOITListViewController: BaseViewController, MOITListPresentable, MOITListViewControllable {
-
+    
     // MARK: - UI
+    private let alarmRootView = UIView()
+    private let pagableAlarmView = PagableCollectionView(frame: .zero)
+    
+    private let listRootView = UIView()
+    
+    
+    private let moitTitleLabel: UILabel = {
+       let label = UILabel()
+        label.text = "전체 스터디"
+        label.textColor = ResourceKitAsset.Color.gray800.color
+        label.font = ResourceKitFontFamily.h6
+        return label
+    }()
+    
+    private let moitCountLabel: UILabel = {
+        let label = UILabel()
+         label.text = "0"
+        label.textColor = ResourceKitAsset.Color.blue600.color
+         label.font = ResourceKitFontFamily.h6
+         return label
+     }()
+    
+    private let emptyMOITView = EmptyMOITView()
+    
+    private let createButton = MOITButton(
+        type: .small,
+        title: "스터디 생성",
+        titleColor: ResourceKitAsset.Color.gray800.color,
+        backgroundColor: ResourceKitAsset.Color.gray200.color
+    )
+    
+    private let participateButton = MOITButton(
+        type: .small,
+        title: "스터디 참여",
+        titleColor: ResourceKitAsset.Color.white.color,
+        backgroundColor: ResourceKitAsset.Color.blue800.color
+    )
     
     // MARK: - Properties
     weak var listener: MOITListPresentableListener?
+    
+    private lazy var moitList: [MOITList] = []
     
     // MARK: - Initializers
     public init(listener: MOITListPresentableListener? = nil) {
@@ -34,9 +77,115 @@ final class MOITListViewController: BaseViewController, MOITListPresentable, MOI
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
-        self.view.backgroundColor = .cyan
-        self.flexRootView.backgroundColor = .systemBlue
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = ResourceKitAsset.Color.gray100.color
+        self.flexRootView.backgroundColor = ResourceKitAsset.Color.gray100.color
+    }
+    
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        configureNavigationBar(
+            leftItems: [.logo],
+            title: "",
+            rightItems: [
+                .alarm,
+                .setting
+            ]
+        )
     }
     
     // MARK: - Methods
+    override func configureConstraints() {
+        super.configureConstraints()
+        
+        flexRootView.flex
+            .paddingHorizontal(20)
+            .paddingTop(20)
+            .define { flex in
+                // 알람
+                flex.addItem(alarmRootView)
+                    .marginBottom(30)
+                
+                // 모잇 몇개인지
+                flex.addItem()
+                    .direction(.row)
+                    .define { flex in
+                        flex.addItem(moitTitleLabel)
+                            .marginRight(4)
+                        flex.addItem(moitCountLabel)
+                    }
+                    .marginBottom(20)
+                
+                // 모잇 리스트
+                flex.addItem(listRootView)
+                    .define({ flex in
+                        flex.addItem(emptyMOITView)
+                            .height(108)
+                            .width(100%)
+                    })
+                
+                // 참여, 생성 버튼
+                flex.addItem()
+                    .direction(.row)
+                    .justifyContent(.spaceBetween)
+                    .define { flex in
+                        flex.addItem(createButton)
+                            .width(47%)
+                        flex.addItem(participateButton)
+                            .width(47%)
+                    }
+            }
+    }
+    
+    override func bind() {
+        super.bind()
+        
+    }
+    
+    func didReceiveMOITList(moitList: [MOITPreviewViewModel]) {
+        print(#function, "previewViewModel: \(moitList)")
+        
+        if moitList.isEmpty { return }
+        
+        moitCountLabel.text = moitList.count.description
+        moitCountLabel.flex.markDirty()
+        
+        emptyMOITView.flex.display(.none)
+        
+        listRootView.flex.define { flex in
+            moitList.forEach {
+                flex.addItem(makeStudyPreview(with: $0))
+                    .height(100)
+                    .width(100%)
+                    .marginBottom(10)
+            }
+        }
+        listRootView.flex.markDirty()
+        listRootView.flex.layout()
+        flexRootView.flex.layout()
+    }
+    
+    func didReceiveAlarm(alarms: [AlarmViewModel]) {
+        
+        self.pagableAlarmView.configure(alarmViewModels: alarms)
+        
+        alarmRootView.flex.define { flex in
+            flex.addItem(pagableAlarmView)
+        }
+        
+        pagableAlarmView.flex.layout()
+        alarmRootView.flex.layout()
+        flexRootView.flex.layout()
+    }
+    
+    private func makeStudyPreview(with viewModel: MOITPreviewViewModel) -> MOITStudyPreview {
+        MOITStudyPreview(
+            remainingDate: viewModel.remainingDate,
+            profileURLString: viewModel.profileUrlString,
+            studyName: viewModel.studyName,
+            studyProgressDescription: viewModel.studyProgressDescription
+        )
+    }
 }
