233f9deeea86e90804dd38bee1c790a12cbdbff6
chansooo <kcs05008@gmail.com>
2023-07-23 19:07:18 +0900

    feat: AlarmViewModel 구현

---
+++ Features/MOITList/MOITListUserInterface/Implement/ViewModel/AlarmViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  AlarmViewModel.swift
+//  MOITListUserInterfaceImpl
+//
+//  Created by kimchansoo on 2023/07/23.
+//  Copyright © 2023 chansoo.MOIT. All rights reserved.
+//
+
+import Foundation
+
+import DesignSystem
+
+public struct AlarmViewModel: Hashable {
+    let alarmType: MOITAlarmType
+    let studyName: String
+}
