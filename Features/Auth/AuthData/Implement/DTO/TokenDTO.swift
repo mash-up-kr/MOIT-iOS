a978640c7f1f7c2223367ee84597748c54d76da5
chansooo <kcs05008@gmail.com>
2023-07-29 23:10:55 +0900

    feat: TokenDTO 구현

---
+++ Features/Auth/AuthData/Implement/DTO/TokenDTO.swift
@@ -0,0 +1,13 @@
+//
+//  EmptyDTO.swift
+//  AuthData
+//
+//  Created by kimchansoo on 2023/07/29.
+//  Copyright © 2023 chansoo.MOIT. All rights reserved.
+//
+
+import Foundation
+
+struct TokenDTO: Decodable {
+    let token: String
+}
