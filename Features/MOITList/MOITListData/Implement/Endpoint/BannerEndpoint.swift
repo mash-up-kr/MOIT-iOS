5316961601177ff8f4fd3b5e3267259d1d424027
chansooo <kcs05008@gmail.com>
2023-07-29 23:13:04 +0900

    feat: Banner 관련 MOITListData 구현

---
+++ Features/MOITList/MOITListData/Implement/Endpoint/BannerEndpoint.swift
@@ -0,0 +1,23 @@
+//
+//  BannerEndpoint.swift
+//  MOITListData
+//
+//  Created by kimchansoo on 2023/07/29.
+//  Copyright © 2023 chansoo.MOIT. All rights reserved.
+//
+
+import Foundation
+
+import MOITListData
+import MOITNetwork
+
+struct BannerEndpoint {
+    
+    static func fetchBannerList() -> Endpoint<BannerDTO> {
+        Endpoint(
+            path: "banner",
+            method: .get
+        )
+    }
+}
+
