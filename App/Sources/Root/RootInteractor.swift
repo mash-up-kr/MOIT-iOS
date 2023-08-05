bc5f78720a6ab91431058b16177ae02b2512a22a
chansooo <kcs05008@gmail.com>
2023-08-06 01:51:46 +0900

    fix: 토큰 하드코딩

--- App/Sources/Root/RootInteractor.swift
+++ App/Sources/Root/RootInteractor.swift
@@ -59,7 +59,8 @@ final class RootInteractor: PresentableInteractor<RootPresentable>,
     
     override func didBecomeActive() {
         super.didBecomeActive()
-//        TokenManagerImpl().delete(key: .authorizationToken)
+        TokenManagerImpl().delete(key: .authorizationToken)
+//        TokenManagerImpl().save(token: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqd3QtdXNlci1kZWZhdWx0IiwiYXVkIjoiYXV0aDB8YWJjQG5hdmVyLmNvbXw3fGRlZmF1bHQiLCJpc3MiOiJodHRwczovL2dpdGh1Yi5jb20vbWFzaC11cC1rci9NT0lULWJhY2tlbmQiLCJpYXQiOjE2ODg4ODkyOTMsImV4cCI6MTY5MTQ4MTI5MywiaW5mbyI6eyJpZCI6NywicHJvdmlkZXJVbmlxdWVLZXkiOiJhdXRoMHxhYmNAbmF2ZXIuY29tIiwibmlja25hbWUiOiJkZWZhdWx0IiwicHJvZmlsZUltYWdlIjowLCJlbWFpbCI6ImFiY0BuYXZlci5jb20iLCJyb2xlcyI6WyJVU0VSIl19fQ.o9WjiGqNOZSkHGDKQ54b50TUEy-oWvPo1-5Egjw1HXc", with: .authorizationToken)
     }
     
     override func willResignActive() {
