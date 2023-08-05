f46e6a5e59481691dda969621e90e22ad2c9c83a
chansooo <89574881+chansooo@users.noreply.github.com>
2023-08-06 01:51:09 +0900

    Feature/moit list 완성 (#99)
    * fix: 로고 변경
    
    * fix: `MOITStudyPreview` 탭 제스쳐 살리기
    
    * feat: `MOIT`에 repeatCycle, dayOfWeeks 타입 string -> enum
    
    * fix: MOITStudyPreview 데이터 정상적으로 보이도록 수정
    
    * fix: MOITStudyPreview 데이터 정상적으로 보이도록 수정한 부분 적용
    
    * fix: `MOITListViewController` 뷰 정상적으로 구현
    
    * feat: attachAlarm 함수 껍데기 구현
    
    * fix: moitDetail id 넘겨주는 로직 수정

--- Features/MOITList/MOITListUserInterface/Implement/ViewModel/MOITPreviewViewModel.swift
+++ Features/MOITList/MOITListUserInterface/Implement/ViewModel/MOITPreviewViewModel.swift
@@ -8,11 +8,13 @@
 
 import Foundation
 
+import MOITListDomain
+
 public struct MOITPreviewViewModel {
     let remainingDate: Int
     let profileUrlString: String?
     let studyName: String
-    let studyProgressDescription: String?
+    var studyProgressDescription: String?
     
     public init(
         remainingDate: Int,
@@ -24,5 +26,77 @@ public struct MOITPreviewViewModel {
         self.profileUrlString = profileUrlString
         self.studyName = studyName
         self.studyProgressDescription = studyProgressDescription
+    }
+    
+    public init(moit: MOIT) {
+        self.remainingDate = moit.dday
+        self.profileUrlString = moit.profileUrl
+        self.studyName = moit.name
+        self.studyProgressDescription = makeStudyProgressDescription(moit: moit)
+    }
+    
+    private func makeStudyProgressDescription(moit: MOIT) -> String {
+        // 시간 포맷 hh:mm:ss -> hh:mm
+        let startTime = removeSeconds(fromTimeString: moit.startTime) ?? moit.startTime
+        let endTime = removeSeconds(fromTimeString: moit.endTime) ?? moit.endTime
+        
+        let repeatCycleDescription = makeRepeatCycleDescription(repeatCycle: moit.repeatCycle)
+        let dayOfWeeksDescription = makeDayOfWeeksDescription(dayOfWeeks: moit.dayOfWeeks)
+        return "\(repeatCycleDescription) \(dayOfWeeksDescription) \(startTime) - \(endTime)"
+    }
+    
+    private func makeDayOfWeeksDescription(dayOfWeeks: [DayOfWeek]) -> String {
+        guard !dayOfWeeks.isEmpty else {
+            return ""
+        }
+        
+        let dayOfWeeksDescription = dayOfWeeks.map { dayOfWeek in
+            switch dayOfWeek {
+            case .monday:
+                return "월요일"
+            case .tuesday:
+                return "화요일"
+            case .wednesday:
+                return "수요일"
+            case .thursday:
+                return "목요일"
+            case .friday:
+                return "금요일"
+            case .saturday:
+                return "토요일"
+            case .sunday:
+                return "일요일"
+            }
+        }
+        return dayOfWeeksDescription.joined(separator: ", ")
+    }
+    
+    
+    private func makeRepeatCycleDescription(repeatCycle: RepeatCycle) -> String {
+        switch repeatCycle {
+        case .none:
+            return ""
+        case .oneWeek:
+            return "매주"
+        case .twoWeek:
+            return "2주마다"
+        case .threeWeek:
+            return "3주마다"
+        case .fourWeek:
+            return "4주마다"
+        }
+    }
+    
+    func removeSeconds(fromTimeString timeString: String) -> String? {
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = "HH:mm:ss"
+        
+        if let date = inputFormatter.date(from: timeString) {
+            let outputFormatter = DateFormatter()
+            outputFormatter.dateFormat = "HH:mm"
+            return outputFormatter.string(from: date)
+        } else {
+            return nil
+        }
     }
 }
