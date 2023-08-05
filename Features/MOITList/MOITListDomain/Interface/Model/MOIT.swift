971807e430056d3b2d4f6e1537b5b538dd0c2a0f
chansooo <kcs05008@gmail.com>
2023-08-06 00:41:04 +0900

    feat: `MOIT`에 repeatCycle, dayOfWeeks 타입 string -> enum

--- Features/MOITList/MOITListDomain/Interface/Model/MOIT.swift
+++ Features/MOITList/MOITListDomain/Interface/Model/MOIT.swift
@@ -13,8 +13,8 @@ public struct MOIT {
     public let name: String
     public let profileUrl: String?
     public let isEnd: Bool
-    public let repeatCycle: String
-    public let dayOfWeeks: [String]
+    public let repeatCycle: RepeatCycle
+    public let dayOfWeeks: [DayOfWeek]
     public let startTime: String
     public let endTime: String
     public let dday: Int
@@ -34,10 +34,65 @@ public struct MOIT {
         self.name = name
         self.profileUrl = profileUrl
         self.isEnd = isEnd
-        self.repeatCycle = repeatCycle
-        self.dayOfWeeks = dayOfWeeks
+        self.repeatCycle = RepeatCycle(rawValue: repeatCycle)
+        self.dayOfWeeks = dayOfWeeks.map { DayOfWeek(rawValue: $0)}
         self.startTime = startTime
         self.endTime = endTime
         self.dday = dday
+    }
+}
+
+public enum RepeatCycle: String {
+    
+    case none = "None"
+    case oneWeek = "ONE_WEEK"
+    case twoWeek = "TWO_WEEK"
+    case threeWeek = "THREE_WEEK"
+    case fourWeek = "FOUR_WEEK"
+    
+    public init(rawValue: String) {
+        switch rawValue {
+        case "ONE_WEEK":
+            self = .oneWeek
+        case "TWO_WEEK":
+            self = .twoWeek
+        case "THREE_WEEK":
+            self = .threeWeek
+        case "FOUR_WEEK":
+            self = .fourWeek
+        default:
+            self = .none
+        }
+    }
+}
+
+public enum DayOfWeek: String {
+    case monday = "MONDAY"
+    case tuesday = "TUESDAY"
+    case wednesday = "WEDNESDAY"
+    case thursday = "THURSDAY"
+    case friday = "FRIDAY"
+    case saturday = "SATURDAY"
+    case sunday = "SUNDAY"
+    
+    public init(rawValue: String) {
+        switch rawValue {
+        case "MONDAY":
+            self = .monday
+        case "TUESDAY":
+            self = .tuesday
+        case "WEDNESDAY":
+            self = .wednesday
+        case "THURSDAY":
+            self = .thursday
+        case "FRIDAY":
+            self = .friday
+        case "SATURDAY":
+            self = .saturday
+        case "SUNDAY":
+            self = .sunday
+        default:
+            self = .monday
+        }
     }
 }
