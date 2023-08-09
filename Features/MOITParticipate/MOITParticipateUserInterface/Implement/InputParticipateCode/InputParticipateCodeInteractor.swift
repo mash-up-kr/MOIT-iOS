93e718c1fa25310a596bc6277e386364c428accf
SongSeoYoung <seoyoung_00@hufs.ac.kr>
2023-08-09 21:33:34 +0900

    feat: 참여할 때 토픽 구독하도록 기능 추가

--- Features/MOITParticipate/MOITParticipateUserInterface/Implement/InputParticipateCode/InputParticipateCodeInteractor.swift
+++ Features/MOITParticipate/MOITParticipateUserInterface/Implement/InputParticipateCode/InputParticipateCodeInteractor.swift
@@ -12,7 +12,7 @@ import MOITParticipateUserInterface
 import MOITParticipateDomain
 import MOITDetail
 import MOITDetailDomain
-
+import FirebaseMessaging
 import RIBs
 import RxSwift
 
@@ -73,6 +73,11 @@ final class InputParticipateCodeInteractor: PresentableInteractor<InputParticipa
 					let moitDetailProfileInfoViewModel = self.convertToMOITDetailProfileInfoViewModel(
 						model: participateEntity
 					)
+                    print("모👀잇 아이디 등록 MOIT-\(participateEntity.moitID)")
+                    Messaging.messaging().subscribe(toTopic: "MOIT-\(participateEntity.moitID)") { error in
+                        print("register topic subscribe error is 👉", error)
+                    }
+
 					self.router?.attachPariticipationSuccess(with: moitDetailProfileInfoViewModel)
 				case .failure:
 					self.presenter.showErrorToast()
