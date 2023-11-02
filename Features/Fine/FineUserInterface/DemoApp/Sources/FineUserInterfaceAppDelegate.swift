fc4627b3206bc868be74c3ec10d824bf6b7c8499
chansooo <kcs05008@gmail.com>
2023-11-02 23:06:44 +0900

    fix: `MOITFine` 관련 DemoApp AppDelegate 구현되지 않은 부분 구현

--- Features/Fine/FineUserInterface/DemoApp/Sources/FineUserInterfaceAppDelegate.swift
+++ Features/Fine/FineUserInterface/DemoApp/Sources/FineUserInterfaceAppDelegate.swift
@@ -20,78 +20,82 @@ import MOITNetworkImpl
 import TokenManagerImpl
 
 import RIBs
+import RxRelay
 
 @main
 final class FineAppDelegate: UIResponder, UIApplicationDelegate {
-	
-	private final class MockFineDependency: FineListDependency {
-		var postFineEvaluateUseCase: PostFineEvaluateUseCase
-		var postMasterAuthorizeUseCase: PostMasterAuthorizeUseCase
-		var compareUserIDUseCase: CompareUserIDUseCase
-		var fetchFineInfoUseCase: FetchFineInfoUseCase
-		var filterMyFineListUseCase: FilterMyFineListUseCase
-		var fetchFineItemUseCase: FetchFineItemUseCase
-		var convertAttendanceStatusUseCase: ConvertAttendanceStatusUseCase
-		
-		init(
-			fetchFineInfoUseCase: FetchFineInfoUseCase,
-			compareUserIDUseCase: CompareUserIDUseCase,
-			filterMyFineListUseCase: FilterMyFineListUseCase,
-			fetchFineItemUseCase: FetchFineItemUseCase,
-			convertAttendanceStatusUseCase: ConvertAttendanceStatusUseCase,
-			postFineEvaluateUseCase: PostFineEvaluateUseCase,
-			postMasterAuthorizeUseCase: PostMasterAuthorizeUseCase
-		) {
-			self.fetchFineInfoUseCase = fetchFineInfoUseCase
-			self.compareUserIDUseCase = compareUserIDUseCase
-			self.filterMyFineListUseCase = filterMyFineListUseCase
-			self.fetchFineItemUseCase = fetchFineItemUseCase
-			self.convertAttendanceStatusUseCase = convertAttendanceStatusUseCase
-			self.postFineEvaluateUseCase = postFineEvaluateUseCase
-			self.postMasterAuthorizeUseCase = postMasterAuthorizeUseCase
-		}
-	}
-	
-	private final class MockFineListener: FineListListener {
-		func fineListViewDidTap(moitID: Int, fineID: Int, isMaster: Bool) {
-			//
-		}
-	}
-	
+    
+    private final class MockFineDependency: FineListDependency {
+        var isMasterPublishRelay: PublishRelay<Bool>
+        
+        var postFineEvaluateUseCase: PostFineEvaluateUseCase
+        var postMasterAuthorizeUseCase: PostMasterAuthorizeUseCase
+        var compareUserIDUseCase: CompareUserIDUseCase
+        var fetchFineInfoUseCase: FetchFineInfoUseCase
+        var filterMyFineListUseCase: FilterMyFineListUseCase
+        var fetchFineItemUseCase: FetchFineItemUseCase
+        var convertAttendanceStatusUseCase: ConvertAttendanceStatusUseCase
+        
+        init(
+            fetchFineInfoUseCase: FetchFineInfoUseCase,
+            compareUserIDUseCase: CompareUserIDUseCase,
+            filterMyFineListUseCase: FilterMyFineListUseCase,
+            fetchFineItemUseCase: FetchFineItemUseCase,
+            convertAttendanceStatusUseCase: ConvertAttendanceStatusUseCase,
+            postFineEvaluateUseCase: PostFineEvaluateUseCase,
+            postMasterAuthorizeUseCase: PostMasterAuthorizeUseCase
+        ) {
+            self.fetchFineInfoUseCase = fetchFineInfoUseCase
+            self.compareUserIDUseCase = compareUserIDUseCase
+            self.filterMyFineListUseCase = filterMyFineListUseCase
+            self.fetchFineItemUseCase = fetchFineItemUseCase
+            self.convertAttendanceStatusUseCase = convertAttendanceStatusUseCase
+            self.postFineEvaluateUseCase = postFineEvaluateUseCase
+            self.postMasterAuthorizeUseCase = postMasterAuthorizeUseCase
+            self.isMasterPublishRelay = PublishRelay<Bool>()
+        }
+    }
+    
+    private final class MockFineListener: FineListListener {
+        func fineListViewDidTap(moitID: Int, fineID: Int, isMaster: Bool) {
+            //
+        }
+    }
+    
     var window: UIWindow?
-	
-	private var router: ViewableRouting?
-	
+    
+    private var router: ViewableRouting?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let window = UIWindow(frame: UIScreen.main.bounds)
         
-		let fetchFineInfoUseCase = FetchFineInfoUseCaseImpl(fineRepository: FineRepositoryImpl(network: NetworkImpl()))
-		let compareUserIDUseCase = CompareUserIDUseCaseImpl(tokenManager: TokenManagerImpl())
-		let filterMyFineListUseCase = FilterMyFineListUseCaseImpl(tokenManager: TokenManagerImpl())
-		let fetchFineItemUseCase = FetchFineItemUseCaseImpl(fineRepository: FineRepositoryImpl(network: NetworkImpl()))
-		let convertAttendanceStatusUseCase = ConvertAttendanceStatusUseCaseImpl()
-		let postFineEvaluateUseCase = PostFineEvaluateUseCaseImpl(repository: FineRepositoryImpl(network: NetworkImpl()))
-		let postMasterAuthorizeUseCase = PostMasterAuthorizeUseCaseImpl(repository: FineRepositoryImpl(network: NetworkImpl()))
-		
-		router = FineListBuilder(
-			dependency: MockFineDependency(
-				fetchFineInfoUseCase: fetchFineInfoUseCase,
-				compareUserIDUseCase: compareUserIDUseCase,
-				filterMyFineListUseCase: filterMyFineListUseCase,
-				fetchFineItemUseCase: fetchFineItemUseCase,
-				convertAttendanceStatusUseCase: convertAttendanceStatusUseCase,
-				postFineEvaluateUseCase: postFineEvaluateUseCase,
-				postMasterAuthorizeUseCase: postMasterAuthorizeUseCase
-			)
-		)
-			.build(withListener: MockFineListener(), moitID: 2)
-		router?.interactable.activate()
-		router?.load()
+        let fetchFineInfoUseCase = FetchFineInfoUseCaseImpl(fineRepository: FineRepositoryImpl(network: NetworkImpl()))
+        let compareUserIDUseCase = CompareUserIDUseCaseImpl(tokenManager: TokenManagerImpl())
+        let filterMyFineListUseCase = FilterMyFineListUseCaseImpl(tokenManager: TokenManagerImpl())
+        let fetchFineItemUseCase = FetchFineItemUseCaseImpl(fineRepository: FineRepositoryImpl(network: NetworkImpl()))
+        let convertAttendanceStatusUseCase = ConvertAttendanceStatusUseCaseImpl()
+        let postFineEvaluateUseCase = PostFineEvaluateUseCaseImpl(repository: FineRepositoryImpl(network: NetworkImpl()))
+        let postMasterAuthorizeUseCase = PostMasterAuthorizeUseCaseImpl(repository: FineRepositoryImpl(network: NetworkImpl()))
+        
+        router = FineListBuilder(
+            dependency: MockFineDependency(
+                fetchFineInfoUseCase: fetchFineInfoUseCase,
+                compareUserIDUseCase: compareUserIDUseCase,
+                filterMyFineListUseCase: filterMyFineListUseCase,
+                fetchFineItemUseCase: fetchFineItemUseCase,
+                convertAttendanceStatusUseCase: convertAttendanceStatusUseCase,
+                postFineEvaluateUseCase: postFineEvaluateUseCase,
+                postMasterAuthorizeUseCase: postMasterAuthorizeUseCase
+            )
+        )
+            .build(withListener: MockFineListener(), moitID: 2)
+        router?.interactable.activate()
+        router?.load()
 
-		window.rootViewController = self.router?.viewControllable.uiviewController
-		window.makeKeyAndVisible()
-		self.window = window
-		
+        window.rootViewController = self.router?.viewControllable.uiviewController
+        window.makeKeyAndVisible()
+        self.window = window
+        
         return true
     }
 }
